34d942a4da3fd57842c96e0ae1ec33f2a3e06d96
DavindHat <davindhat1@gmail.com>
2024-12-26 14:08:30 +0300

    db_update_commit

---
+++ bob_sue_DB.py
@@ -0,0 +1,19 @@
+import shelve
+bob = {'name': 'Bob Smith', 'age': 42, 'pay': 30000, 'job': 'dev'}
+sue = {'name': 'Sue jones', 'age': 45, 'pay': 40000, 'job': 'hdw'}
+tom = {'name': 'Tom', 'age': 50, 'pay': 0, 'job': 'None'}
+
+#DB
+db = {}
+db['bob'] = bob
+db['sue'] = sue
+db['tom'] = tom
+
+if __name__ == '__main__':
+    for key in db:
+        print(key, '=>\n', db[key])
+
+db = shelve.open('people-shelve')
+db['bob'] = bob
+db['sue'] = sue
+db.close()
