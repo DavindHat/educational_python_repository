2bbb78ebdb874d8e2a0c6303b90fb7a481a1f654
DavindHat <123246781+DavindHat@users.noreply.github.com>
2025-01-26 16:27:35 +0300

    update bob_sue_DB.py

--- bob_sue_DB.py
+++ bob_sue_DB.py
@@ -3,7 +3,7 @@ bob = {'name': 'Bob Smith', 'age': 42, 'pay': 30000, 'job': 'dev'}
 sue = {'name': 'Sue jones', 'age': 45, 'pay': 40000, 'job': 'hdw'}
 tom = {'name': 'Tom', 'age': 50, 'pay': 0, 'job': 'None'}
 
-#DB
+#DB test
 db = {}
 db['bob'] = bob
 db['sue'] = sue
